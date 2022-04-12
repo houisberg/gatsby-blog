bd37b341be759b762df62aa5faf7bfcd00c4af3f
houisberg <dropshiboru@gmail.com>
2022-04-12 21:53:36 +0900

    ヘッダー修正

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,11 @@
+import { Link } from "gatsby"
 import React from "react"
 import "../styles/header.css"
 
 export default function Header() {
     return (
         <header className="header">
-            <h1>Newport 1963</h1>
+            <h1><Link to="/">Newport 1963</Link></h1>
             <span>備忘録のようなもの。Twitter→<a href="https://twitter.com/jathinus" rel="noopener noreferrer" target="_blank">@Jathinus</a></span>
         </header>
     )
